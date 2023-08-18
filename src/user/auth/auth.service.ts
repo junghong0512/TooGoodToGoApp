7dad6c963618f0479907b36e0611909d1ba1d5a5
Jung Hong <junghong@ninewatt.com>
2023-08-18 11:20:42 +0900

    [UPDATE] 회원가입/로그인 API Using JWT

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 import { UserType } from '@prisma/client';
 
@@ -9,6 +10,11 @@ interface SignupParams {
   password: string;
   name: string;
   phone: string;
+}
+
+interface SigninParams {
+  email: string;
+  password: string;
 }
 
 @Injectable()
@@ -36,6 +42,37 @@ export class AuthService {
       },
     });
 
-    return user;
+    return await this.generateJWT(name, user.id);
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new HttpException('Invalid credentials', 400);
+    }
+
+    const hashedPassword = user.password;
+
+    const isValidPassword = bcrypt.compare(password, hashedPassword); // 순서 중요
+
+    if (!isValidPassword) {
+      /* HttpException 사용하는 이유: 구체적인 로그인 실패 사유를 제공하지 않기 위해 */
+      throw new HttpException('Invalid credentials', 400);
+    }
+
+    return await this.generateJWT(user.name, user.id);
+  }
+
+  private generateJWT(name: string, id: number) {
+    return jwt.sign(
+      {
+        name,
+        id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: 360000 },
+    );
   }
 }
